59b74fe29b6327d4250f5fafc9afbb040f829e60
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-03 12:50:58 +0500

    App Updated

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { useState , useEffect } from 'react'
+import calculateTimer from '../../Helper/CalculateTimer'
 import './Main.css'
-import { useState } from 'react'
 
 const Main = () => {
 
     const [timeInSeconds , setTimeInSeconds] = useState<number>(0);
+    const [timerArray , setTimerArray] = useState<Array<number>>([]);
+
+    useEffect(() => {
+        let timeArray: Array<number> = calculateTimer(timeInSeconds);
+        setTimerArray(timeArray);
+    }, [timeInSeconds]);
 
     return (
         <section className="time-container">
-            <p className="timer-text">0</p>
+            <p className="timer-text">{timerArray[0]}</p>
             <span>:</span>
-            <p className="timer-text">0</p>
+            <p className="timer-text">{timerArray[1]}</p>
             <span>:</span>
-            <p className="timer-text">0</p>
+            <p className="timer-text">{timerArray[2]}</p>
         </section>
     )
 }
