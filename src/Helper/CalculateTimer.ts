d72157db4744103501fff826cb78e6090dc5e970
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-03 12:39:14 +0500

    App Updated

---
+++ src/Helper/CalculateTimer.ts
@@ -0,0 +1,8 @@
+function calculateTimer(timeInSeconds: number): Array<number> {
+  let hours: number = Math.floor(timeInSeconds / 3600);
+  let minutes: number = Math.floor((timeInSeconds - hours * 3600) / 60);
+  let seconds: number = timeInSeconds - hours * 3600 - minutes * 60;
+
+  return [hours, minutes, seconds];
+}
+export default calculateTimer;
