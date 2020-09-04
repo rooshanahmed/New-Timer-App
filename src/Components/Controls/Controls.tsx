00084223578b59ace37a121d32bc8bd3e47c2c8e
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-04 12:24:49 +0500

    App Updated

--- src/Components/Controls/Controls.tsx
+++ src/Components/Controls/Controls.tsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react'
-import './Controls.css'
+import React, { useState } from "react";
+import "./Controls.css";
 
 type Prop = {
-    setTimeInSeconds: Function
-}
+  setTimeInSeconds: Function;
+};
 
-const Controls = (props:Prop) => {
+const Controls = (props: Prop) => {
+  const { setTimeInSeconds } = props;
+  const [intervalId, setIntervalId] = useState<number>(0);
 
-    const { setTimeInSeconds } = props;
-    const [intervalId , setIntervalId] = useState<number>(0)
+  const handlePlayButton = () => {
+    let interval: any = setInterval(() => {
+      setTimeInSeconds((previousState: number) => previousState + 1);
+    }, 1000);
+    setIntervalId(interval);
+  };
 
-    const handlePlayButton = () => {
-        let interval:any = setInterval(() => {
-            setTimeInSeconds((previousState:number) => previousState + 1)
-        }, 1000)
-        setIntervalId(interval)
-    }
+  const handleStopButton = () => {
+    clearInterval(intervalId);
+  };
 
-    const handleStopButton = () => {
-        clearInterval(intervalId)
-    }
+  const handleResetButton = () => {
+    clearInterval(intervalId);
+    setTimeInSeconds(0);
+  };
 
-    const handleResetButton = () => {
-        clearInterval(intervalId);
-        setTimeInSeconds(0);
-    }
+  return (
+    <section className="container-control">
+      <button onClick={handlePlayButton}>START</button>
+      <button onClick={handleStopButton}>STOP</button>
+      <button onClick={handleResetButton}>RESET</button>
+    </section>
+  );
+};
 
-    return (
-        <section className="container-control">
-            <button onClick={handlePlayButton}>START</button>
-            <button onClick={handleStopButton}>STOP</button>
-            <button onClick={handleResetButton}>RESET</button>
-        </section>
-    )
-}
-
-export default Controls
+export default Controls;
